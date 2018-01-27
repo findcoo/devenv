723eeeca031cfd9560dbc66cfb5d7f8bb079eed8
findcoo <thirdlif2@gmail.com>
2018-01-27 22:44:59 +0900

    update

---
+++ .custom_profile.sh
@@ -0,0 +1,27 @@
+# alias commands
+alias dop='docker-compose'
+alias pytest='python -m unittest'
+
+# pyenv
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# go
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+
+# neovim
+export EDITOR=nvim
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export PATH=$PATH:$GOROOT/bin
+
+alias vim=nvim
